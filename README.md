ba8a7034585c94523c75aa4a2212937a624a2084
NALINKAYAL <nvkayal_b21@el.vjti.ac.in>
2023-02-21 23:37:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This a basic website which shows the people having the birthday(not actual)  built using some tools of react
---
+++ docs/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Aman Gupta",
+    age: 40,
+    image:
+      "https://pbs.twimg.com/profile_images/1480461625727455234/fofDHTWQ_400x400.jpg",
+  },
+  {
+    id: 2,
+    name: "Anupam  Mittal",
+    age: 51,
+    image:
+      "https://images.hindustantimes.com/img/2022/01/25/1600x900/anupam_mittal_1643097182557_1643097192769.PNG",
+  },
+  {
+    id: 3,
+    name: "Piyush Bansal",
+    age: 36,
+    image:
+      "https://assets.entrepreneur.com/content/3x2/2000/1674100659-pbansal.jpg",
+  },
+  {
+    id: 4,
+    name: "Ashneer Grover",
+    age: 40,
+    image:
+      "https://images.assettype.com/fortuneindia%2F2022-01%2Fea0ee46d-8917-44f1-88c8-6e3c4586ea04%2FAshneer_Grover_BharatPe.jpg?w=1250&q=60",
+  },
+  {
+    id: 5,
+    name: "Vineeta Singh",
+    age: 38,
+    image:
+      "https://pbs.twimg.com/profile_images/1473706332104663048/mYM4x-7Q_400x400.jpg",
+  },
+];
